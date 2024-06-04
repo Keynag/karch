6644dda34d0215ece46da148dfae28b11210a5d4
Keyn73 <82419402+Keynag@users.noreply.github.com>
2024-06-04 23:15:36 +0300

    K >>k

--- wm/sway/sway.sh
+++ wm/sway/sway.sh
@@ -2,5 +2,5 @@
 
 sudo pacman -S sway swaybg xdg-desktop-portal-gtk jq lxsession-gtk3 ly --needed && sudo systemctl enable ly;
 paru -S autotiling;
-cp -r ~/Karch/wm/sway/sway ~/.config/ && echo "SWAY POSTAVLEN :)"
+cp -r ~/karch/wm/sway/sway ~/.config/ && echo "SWAY POSTAVLEN :)"
 
