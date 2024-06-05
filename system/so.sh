3c88dec7c46430c5dbee357b63d48264e01fb33a
Keyn73 <82419402+Keynag@users.noreply.github.com>
2024-06-05 13:31:06 +0300

    Update so.sh

--- system/so.sh
+++ system/so.sh
@@ -5,13 +5,13 @@ mkdir tools && cd tools;
 git clone https://aur.archlinux.org/alhp-keyring.git && git clone https://aur.archlinux.org/alhp-mirrorlist.git;
 git clone https://aur.archlinux.org/upd72020x-fw.git && git clone https://aur.archlinux.org/paru.git;
 git clone https://aur.archlinux.org/ananicy-cpp.git && git clone https://aur.archlinux.org/cachyos-ananicy-rules-git.git
-git clone https://aur.archlinux.org/xpadneo-dkms.git && cd ~ && gpg --import karch/systems/anonfunc.asc;
+git clone https://aur.archlinux.org/xpadneo-dkms.git && gpg --import ~/karch/system/anonfunc.asc;
 
 sudo pacman-key --init && sudo pacman-key --populate;
 
-sudo pacman -S llvm clang lld mold openmp;
+sudo pacman -S llvm clang lld mold openmp --needed;
 
-sudo cp -r karch/systems/makepkg.conf /etc/makepkg.conf;
+sudo cp -r ~/karch/system/makepkg.conf /etc/makepkg.conf;
 
 cd ~/tools/alhp-keyring && makepkg -sric;
 
@@ -29,17 +29,17 @@ cd ~/tools/cachyos-ananicy-rules-git && makepkg -sric;
 
 sudo systemctl enable --now ananicy-cpp;
 
-cd ~ && sudo cp -r karch/system/pacman.conf /etc/pacman.conf;
+sudo cp -r ~/karch/system/pacman.conf /etc/pacman.conf;
 
-sudo cp -r karch/system/environment /etc/environment;
+sudo cp -r ~/karch/system/environment /etc/environment;
 
-sudo cp -r karch/system/geoclue.conf /etc/geoclue/geoclue.conf;
+sudo cp -r ~/karch/system/geoclue.conf /etc/geoclue/geoclue.conf;
 
 sudo systemctl mask NetworkManager-wait-online.service;
 
-sudo rm -r ~/tools/ && pacman -Syyuu;
+cd ~ && sudo rm -r ~/tools/ && pacman -Syyuu;
 
-echo "ALL GOOD KEYN73 :)" && shutdown -r now
+echo "ALL GOOD KEYN73 :)" && shutdown -r
 
 
 
